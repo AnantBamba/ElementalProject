5ff9a109b919554dbb59c49e4c531a3729b171dc
Anant Bamba <anant.bamba@gmail.com>
2025-03-31 13:39:28 -0700

    fixing water

--- Assets/Scripts/Water/WaterController.cs
+++ Assets/Scripts/Water/WaterController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class WaterController : MonoBehaviour
 {
     public Transform waterPlane;
-    public float targetHeight = 5f; // Maximum tide level
+    public float targetHeight = 0f; // Maximum tide level
     public float normalHeight = 0f; // Default water level
     public float speed = 2f; // Adjust speed for smooth transition
 
